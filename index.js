e6b7d9b139aa002c49622e462cb37421a6121ac4
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-12 01:36:29 +0300

    add: cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(cors());
 
 const BACKEND_PORT = 5000;
 
 const productRouter = require("./routes/product.routes");
-
-const app = express();
-app.use(express.json()); // for parsing application/json
 
 app.use("/api", productRouter);
 
