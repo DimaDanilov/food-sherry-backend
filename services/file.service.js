bfababbffd912e4c700ecd0ce99a4ec989f14fed
Dima Danilov <dima.danilov.2012@mail.ru>
2023-04-13 19:40:03 +0300

    add: 1 or multiple images fix bug

--- services/file.service.js
+++ services/file.service.js
@@ -3,15 +3,22 @@ const path = require("path");
 const fs = require("fs");
 
 class FileService {
+  _saveFile(file) {
+    const fileName = uuid.v4() + ".jpg";
+    const filePath = path.resolve("static/food_images", fileName);
+    file.mv(filePath);
+    return fileName;
+  }
   saveFiles(files) {
     try {
       let filePaths = [];
-      files.forEach((file) => {
-        const fileName = uuid.v4() + ".jpg";
-        const filePath = path.resolve("static/food_images", fileName);
-        file.mv(filePath);
-        filePaths.push(fileName);
-      });
+      if (Array.isArray(files)) {
+        files.forEach((file) => {
+          filePaths.push(this._saveFile(file));
+        });
+      } else {
+        filePaths.push(this._saveFile(files));
+      }
       return filePaths;
     } catch (e) {
       console.log(e);
